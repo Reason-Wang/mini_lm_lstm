82256416a536221128f1ef20fd397aae1ff02bbd
RenxiWang <72866053+Reason-Wang@users.noreply.github.com>
2021-11-13 11:49:04 +0800

    Add files via upload

---
+++ data/process.py
@@ -0,0 +1,56 @@
+
+def make_batch(train_path, word2number_dict, batch_size, n_step):
+    all_input_batch = []
+    all_target_batch = []
+
+    text = open(train_path, 'r', encoding='utf-8') #open the file
+
+    input_batch = []
+    target_batch = []
+    for sen in text:
+        word = sen.strip().split(" ")  # space tokenizer
+        word = ["<sos>"] + word
+        word = word + ["<eos>"]
+
+        if len(word) <= n_step:   #pad the sentence
+            word = ["<pad>"]*(n_step+1-len(word)) + word
+
+        for word_index in range(len(word)-n_step):
+            input = [word2number_dict[n] for n in word[word_index:word_index+n_step]]  # create (1~n-1) as input
+            target = word2number_dict[word[word_index+n_step]]  # create (n) as target, We usually call this 'casual language model'
+            input_batch.append(input)
+            target_batch.append(target)
+
+            if len(input_batch) == batch_size:
+                all_input_batch.append(input_batch)
+                all_target_batch.append(target_batch)
+                input_batch = []
+                target_batch = []
+
+    return all_input_batch, all_target_batch  # (batch num, batch size, n_step) (batch num, batch size)
+
+def make_dict(train_path):
+    text = open(train_path, 'r', encoding='utf-8')  #open the train file
+    word_list = set()  # a set for making dict
+
+    for line in text:
+        line = line.strip().split(" ")
+        word_list = word_list.union(set(line))
+
+    word_list = list(sorted(word_list))   #set to list
+
+    word2number_dict = {w: i+2 for i, w in enumerate(word_list)}
+    number2word_dict = {i+2: w for i, w in enumerate(word_list)}
+
+    #add the <pad> and <unk_word>
+    word2number_dict["<pad>"] = 0
+    number2word_dict[0] = "<pad>"
+    word2number_dict["<unk_word>"] = 1
+    number2word_dict[1] = "<unk_word>"
+    word2number_dict["<sos>"] = 2
+    number2word_dict[2] = "<sos>"
+    word2number_dict["<eos>"] = 3
+    number2word_dict[3] = "<eos>"
+
+    return word2number_dict, number2word_dict
+
